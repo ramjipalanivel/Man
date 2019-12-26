6d7698df8fddd701e2ca7994ebd8118a15607456
ramjipalanivel <34581698+ramjipalanivel@users.noreply.github.com>
2019-12-26 11:25:28 +0530

    Add files via upload

---
+++ Arithmetic Operators.py
@@ -0,0 +1,22 @@
+a = 21
+b = 10
+c = 0
+c = a + b
+print ("Value of c is ", c)
+c = a - b
+print ("Value of c is ", c )
+c = a * b
+print ("Value of c is ", c)
+c = a / b
+print ("Value of c is ", c )
+c = a % b
+print ("Value of c is ", c)
+a = 2
+b = 3
+c = a**b
+print ("Value of c is ", c)
+a = 10
+b = 5
+c = a//b
+print ("Value of c is ", c)
+
